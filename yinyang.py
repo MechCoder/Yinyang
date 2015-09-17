607635dc9d9365a8a05b485662ecd92871475739
MechCoder <manojkumarsivaraj334@gmail.com>
2015-09-17 17:45:12 -0400

    Rename YinYang to YinYangKMeans

--- yinyang.py
+++ yinyang.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClusterMixin
 from sklearn.utils.extmath import row_norms
 
-class Yinyang(BaseEstimator, ClusterMixin):
+class YinyangKMeans(BaseEstimator, ClusterMixin):
     """
     Scikit-learn compatible K-Means clusterer based on
     http://jmlr.org/proceedings/papers/v37/ding15.pdf
@@ -18,10 +18,10 @@ class Yinyang(BaseEstimator, ClusterMixin):
         self.random_state = random_state
 
     def fit(self, X):
-        rng = np.random.RandomState(0)
+        rng = np.random.RandomState(self.random_state)
         new_cluster_centers = np.zeros((self.n_clusters, X.shape[1]))
-        shifted_samples = np.zeros(self.n_clusters)
-        sample_array = np.arange(X.shape[0])
+        n_samples_arrays = np.arange(X.shape[0])
+
         if self.n_clusters > 20:
             raise ValueError("Group clustering not supported yet")
 
@@ -31,6 +31,7 @@ class Yinyang(BaseEstimator, ClusterMixin):
             raise ValueError("wait till we support other initializations.")
 
         # Run K-Means for the first time.
+        # Don't do cluster.KMeans().fit(X) because of input_validation etc.
         dot_product = 2 * np.dot(X, old_cluster_centers_.T)
         cluster_norms = row_norms(old_cluster_centers_, squared=True).reshape(1, -1)
         self.distances_ = row_norms(X, squared=True).reshape(-1, 1) - dot_product + cluster_norms
@@ -39,7 +40,7 @@ class Yinyang(BaseEstimator, ClusterMixin):
         upper_and_lower_bounds = np.argpartition(self.distances_, 1, axis=1)
         self.labels_ = upper_and_lower_bounds[:, 0]
         self.almost_labels_ = upper_and_lower_bounds[:, 1]
-        self.upper_and_lower_bounds = self.distances_[sample_array.reshape(-1, 1), upper_and_lower_bounds]
+        self.upper_and_lower_bounds_ = self.distances_[n_samples_arrays.reshape(-1, 1), upper_and_lower_bounds]
 
         # Update cluster centers
         for i in range(self.n_clusters):
@@ -57,39 +58,40 @@ class Yinyang(BaseEstimator, ClusterMixin):
             # Add the drift to the upper bounds and subtract the drift from the lower bounds.
             for i in range(self.n_clusters):
                 mask = self.labels_ == i
-                self.upper_and_lower_bounds[:, 0][mask] += drift[i]
-                self.upper_and_lower_bounds[:, 1][mask] -= drift[i]
+                self.upper_and_lower_bounds_[:, 0][mask] += drift[i]
+                self.upper_and_lower_bounds_[:, 1][mask] -= drift[i]
 
             # If the previously second_largest_bound is now lesser than the largest bound
             # set the upper bound to the distance between the largest_bound
             # This is based on d(old_center, new_center) + d(old_center, X) > d(X, new_center)
-            mask_changed_bounds = self.upper_and_lower_bounds[:, 1] < self.upper_and_lower_bounds[:, 0]
+            mask_changed_bounds = self.upper_and_lower_bounds_[:, 1] < self.upper_and_lower_bounds_[:, 0]
 
             #XXX: Vectorize?
             for i in range(self.n_clusters):
                 cluster = self.cluster_centers_[i]
                 new_mask = np.logical_and(mask_changed_bounds, self.labels_ == i)
                 distances = np.sum((X[new_mask] - cluster)**2, axis=1)
-                self.upper_and_lower_bounds[:, 0][new_mask] = distances
+                self.upper_and_lower_bounds_[:, 0][new_mask] = distances
 
             # Now we can be sure that the second closest center is actually the closest.
             # Reassign the labels.
-            mask_changed_bounds = self.upper_and_lower_bounds[:, 1] < self.upper_and_lower_bounds[:, 0]
+            mask_changed_bounds = self.upper_and_lower_bounds_[:, 1] < self.upper_and_lower_bounds_[:, 0]
             tmp = self.labels_[mask_changed_bounds]
             self.labels_[mask_changed_bounds] = self.almost_labels_[mask_changed_bounds]
             self.almost_labels_[mask_changed_bounds] = tmp
 
-            self.upper_and_lower_bounds[:, 1][mask_changed_bounds] = self.upper_and_lower_bounds[:, 0][mask_changed_bounds]
+            self.upper_and_lower_bounds_[:, 1][mask_changed_bounds] = self.upper_and_lower_bounds_[:, 0][mask_changed_bounds]
 
             #XXX: Vectorize?
             for i in range(self.n_clusters):
                 cluster = self.cluster_centers_[i]
                 new_mask = np.logical_and(mask_changed_bounds, self.labels_ == i)
-                shifted_samples[i] = np.count_nonzero(new_mask)
                 distances = np.sum((X[new_mask] - cluster)**2, axis=1)
-                self.upper_and_lower_bounds[:, 0][new_mask] = distances
+                self.upper_and_lower_bounds_[:, 0][new_mask] = distances
 
             # TODO: Optimize this step.
             for i in range(self.n_clusters):
                 mask = self.labels_ == i
                 self.cluster_centers_[i] = np.mean(X[mask], axis=0)
+
+        self.n_iter_ = n_iter
